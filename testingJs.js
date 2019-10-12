a589fee58aff06c44dd0de1508c3516b5f86fe62
AJ-Scully <aaronscully2@gmail.com>
2019-10-13 00:10:11 +0100

    Testing Basic Nutritional Data

--- testingJs.js
+++ testingJs.js
@@ -4,11 +4,14 @@ $(document).ready(function() {
 
 function testy(food){
     //alert(food);
+    console.clear();
     $('#foodName').html(food);
 
     var urlFood=encodeURI(food);
     $.get( "https://api.edamam.com/api/food-database/parser?ingr="+urlFood+"&app_id=9ccfd3ea&app_key=422e0ba66ae6c563f47a9fe391a437f0", function( data, status) {
+        console.log("----------Response JSON----------");
         console.log(data);
+        console.log("---------------------------------------");
         var obj=data;
         //var objs;
         //console.log(data.hints[0].measures[1].label);
@@ -17,7 +20,9 @@ function testy(food){
         $.each(obj, function (key, value) {
             if(key=="hints"){
                 //objs=value[0];
+                console.log("----------Food JSON----------");
                 console.log(value[0]);
+                console.log("-----------------------------");
                 foodId=value[0].food.foodId;
                 console.log("Food Id: "+foodId);
             }
@@ -36,6 +41,7 @@ function testy(food){
                     "quantity": 100,
                     "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_gram",
                     "foodId": foodId,
+                    "qualifiers": [ "http://www.edamam.com/ontologies/edamam.owl#Qualifier_large" ]
                 }
             ]
         };
@@ -45,12 +51,27 @@ function testy(food){
             data :JSON.stringify(nutrientReq),
             url: "https://api.edamam.com/api/food-database/nutrients?app_id=9ccfd3ea&app_key=422e0ba66ae6c563f47a9fe391a437f0",
             contentType: "application/json",
-            success: function(glyc){
-                console.log(glyc);
+            success: function(fullNutrData){
+                console.log("----------Nutrient JSON----------");
+                console.log(fullNutrData);
+                console.log("---------------------------------");
                 //$("#result").html(glyc);
+
+                fullNutrData=fullNutrData.totalNutrients;
+                $.each(fullNutrData,function (key,value) {
+
+                    if(key==""){
+
+                    }else if(key==""){
+
+                    }else if(key==""){
+
+                    }else if(key==""){
+
+                    }
+                });
             }
         });
-
 
         //var testyJSON=JSON.parse(data);
         //console.log(testyJSON);
